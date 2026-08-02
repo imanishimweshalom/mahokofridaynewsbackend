f47e53b8618c642b66f464a621615950da64652c
shalom250 <imanishimweshalom@gmail.com>
2026-08-02 16:24:38 +0200

    upload this

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ const PORT = process.env.PORT || 5000;
 
 // Security middleware
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://mahokofridaynews.onrender.com"
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: allowedOrigins,
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
